00199a1a43ab2ef2e97a13491cd8064f706218f1
Neil M <neilm813@gmail.com>
2021-09-02 10:13:05 -0700

    algos

--- algorithms/sorts/partition.js
+++ algorithms/sorts/partition.js
@@ -23,6 +23,7 @@
   cases."
   https://stackoverflow.com/questions/164163/quicksort-choosing-the-pivot
 */
+
 const nums1 = [11, 8, 14, 3, 6, 2, 7];
 const nums2 = [11, 8, 14, 3, 3, 3, 6, 2, 7];
 const nums3 = [1, 17, 12, 3, 9, 13, 21, 4, 27];
@@ -40,4 +41,58 @@ const nums3 = [1, 17, 12, 3, 9, 13, 21, 4, 27];
  *    being processed.
  * @returns {Array<number>} The idx where left section of smaller items ends.
  */
-function partition(nums = [], left = 0, right = nums.length - 1) {}
+function partition(nums, left = 0, right = nums.length - 1) {
+  const midIdx = Math.floor((left + right) / 2);
+  const pivotVal = nums[midIdx];
+  let leftIdx = left;
+  let rightIdx = right;
+
+  while (true) {
+    while (nums[leftIdx] < pivotVal) {
+      // It's already smaller, so it's in the right spot, skip it.
+      leftIdx += 1;
+    }
+
+    while (nums[rightIdx] > pivotVal) {
+      // it's already larger, it's in the right spot, skip it.
+      rightIdx -= 1;
+    }
+
+    if (leftIdx >= rightIdx) {
+      return rightIdx;
+    }
+
+    // Destructure swap syntax to swap the item at leftIdx with the item at rightIdx
+    [nums[leftIdx], nums[rightIdx]] = [nums[rightIdx], nums[leftIdx]];
+
+    leftIdx += 1;
+    rightIdx -= 1;
+  }
+}
+
+module.exports = {
+  partition,
+};
+
+/**
+ * The lomuto partition scheme does on average 3x more swaps than Hoare's
+ * scheme.
+ * @param {Array<number>} nums
+ * @param {number} low Start of section to partition.
+ * @param {number} hi End of section to partition.
+ */
+function partitionLomuto(nums = [], low = 0, hi = nums.length - 1) {
+  const pivot = nums[hi];
+  let i = low - 1;
+
+  for (let j = low; j < hi; j++) {
+    if (nums[j] <= pivot) {
+      i += 1;
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+  }
+
+  // final swap of pivot into correct position
+  [nums[i + 1], nums[hi]] = [nums[hi], nums[i + 1]];
+  return i + 1;
+}
