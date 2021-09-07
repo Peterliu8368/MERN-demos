0810c5220b07935a31ea6965d4fa0dd889a5b2b0
Neil M <neilm813@gmail.com>
2021-09-07 16:05:56 -0700

    render basic table

--- interview-challenge-fetch-btn/src/components/UsersTable.js
+++ interview-challenge-fetch-btn/src/components/UsersTable.js
@@ -3,7 +3,30 @@ const UsersTable = ({ users }) => {
     return null;
   }
 
-  return users.map((user) => <p key={user.id}>{user.name}</p>);
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Name</th>
+          <th>Username</th>
+          <th>Email</th>
+          <th>Address</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user, i) => (
+          <tr key={user.id}>
+            <td>{i + 1}</td>
+            <td>{user.name}</td>
+            <td>{user.username}</td>
+            <td>{user.email}</td>
+            <td></td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default UsersTable;
