9b1973709915c7fa84d4747b4468bf027ed77799
Neil M <neilm813@gmail.com>
2021-09-07 16:19:47 -0700

    add loading spinner

--- interview-challenge-fetch-btn/src/App.js
+++ interview-challenge-fetch-btn/src/App.js
@@ -8,8 +8,11 @@ import DisplayError from "./components/Error";
 function App() {
   const [users, setUsers] = useState(null);
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const handleFetchClick = (e) => {
+    setLoading(true);
+
     // fetch("http://httpstat.us/404")
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((resp) => {
@@ -25,6 +28,11 @@ function App() {
       .catch((err) => {
         console.log(err);
         setError(err);
+      })
+      .finally(() => {
+        setTimeout(() => {
+          setLoading(false);
+        }, 500);
       });
   };
 
@@ -33,17 +41,27 @@ function App() {
   // If this were a larger project, I would use a CSS-in-js library instead of inline-styles.
   return (
     <div style={{ maxWidth: "85%", margin: "0 auto" }}>
-      <button
-        onClick={handleFetchClick}
-        style={{
-          padding: 5,
-          fontSize: 22,
-          marginTop: "2rem",
-          marginBottom: "2rem",
-        }}
-      >
-        {fetchBtnText}
-      </button>
+      {loading ? (
+        <img
+          src="https://i.gifer.com/ZZ5H.gif"
+          alt="Loading"
+          height="40px"
+          width="40px"
+          style={{ marginTop: "2rem", marginBottom: "2rem" }}
+        />
+      ) : (
+        <button
+          onClick={handleFetchClick}
+          style={{
+            padding: 5,
+            fontSize: 22,
+            marginTop: "2rem",
+            marginBottom: "2rem",
+          }}
+        >
+          {fetchBtnText}
+        </button>
+      )}
 
       <DisplayError error={error} />
       <UsersTable users={users} />
