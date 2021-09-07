97ec96352f2ebfd7d88c786a1684f41b79472fc7
Neil M <neilm813@gmail.com>
2021-09-07 10:09:21 -0700

    algos

--- algorithms/arrays/orderedIntersection.js
+++ algorithms/arrays/orderedIntersection.js
@@ -31,4 +31,26 @@ const expected3 = [];
  *    sorted and contains only the shared values between the two arrays
  *    deduped.
  */
-function orderedIntersection(sortedA, sortedB) {}
+function orderedIntersection(sortedA, sortedB) {
+  let idxA = 0;
+  let idxB = 0;
+
+  const intersection = [];
+
+  while (idxA < sortedA.length && idxB < sortedB.length) {
+    if (sortedA[idxA] === sortedB[idxB]) {
+      if (intersection[intersection.length - 1] !== sortedA[idxA]) {
+        // add it only if the last num added isn't the same num
+        intersection.push(sortedA[idxA]);
+      }
+      idxA++;
+      idxB++;
+    } else if (sortedA[idxA] < sortedB[idxB]) {
+      idxA++;
+      // Not === and not less than, so it's greater.
+    } else {
+      idxB++;
+    }
+  }
+  return intersection;
+}
